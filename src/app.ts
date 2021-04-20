e12a43fbee8f32d712c3881ded550b4b8e73d514
DiegoVictor <diegovictorgonzaga@gmail.com>
2021-04-20 18:21:56 -0300

    Configure middlewares

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,11 @@
 import express from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
+
 
 const app = express();
 
+app.use(cors());
+app.use(helmet());
+app.use(express.json());
 export default app;
