4966cc2c9c0a409a36e97f54f463a33a64a54fa8
DiegoVictor <diegovictorgonzaga@gmail.com>
2021-04-20 18:20:26 -0300

    Create settings model

---
+++ src/entities/Settings.ts
@@ -0,0 +1,34 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { v4 as uuid } from 'uuid';
+
+@Entity('settings')
+class Setting {
+  @PrimaryColumn()
+  id: string;
+
+  @Column()
+  username: string;
+
+  @Column()
+  chat: boolean;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+
+  constructor() {
+    if (!this.id) {
+      this.id = uuid();
+    }
+  }
+}
+
+export default Setting;
