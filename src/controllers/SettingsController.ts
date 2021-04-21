46160d1e99608d8581c9b7a999751506f3b49455
DiegoVictor <diegovictorgonzaga@gmail.com>
2021-04-21 13:08:10 -0300

    Move logic to service

--- src/controllers/SettingsController.ts
+++ src/controllers/SettingsController.ts
@@ -1,19 +1,12 @@
 import { Request, Response } from 'express';
-import { getCustomRepository } from 'typeorm';
-
-import SettingsRepository from '../repositories/SettingsRepository';
+import SettingsService from '../services/SettingsService';
 
 class SettingsController {
   async store(request: Request, response: Response): Promise<Response> {
     const { username, chat } = request.body;
 
-    const settingsRepository = getCustomRepository(SettingsRepository);
-    const settings = settingsRepository.create({
-      username,
-      chat,
-    });
-
-    await settingsRepository.save(settings);
+    const settingsService = new SettingsService();
+    const settings = await settingsService.store({ username, chat });
 
     return response.json(settings);
   }
---
+++ src/services/SettingsService.ts
@@ -0,0 +1,30 @@
+import { badRequest } from '@hapi/boom';
+import { getCustomRepository } from 'typeorm';
+
+import SettingsRepository from '../repositories/SettingsRepository';
+
+interface ISettingsCreate {
+  chat: boolean;
+  username: string;
+}
+
+class SettingsService {
+  async store({ username, chat }: ISettingsCreate) {
+    const settingsRepository = getCustomRepository(SettingsRepository);
+
+    if (await settingsRepository.findOne({ username })) {
+      throw badRequest('User already exists', { code: 140 });
+    }
+
+    const settings = settingsRepository.create({
+      username,
+      chat,
+    });
+
+    await settingsRepository.save(settings);
+
+    return settings;
+  }
+}
+
+export default SettingsService;
