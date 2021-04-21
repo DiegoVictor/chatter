7e64030bce9cd572a46b680c764c134057209196
DiegoVictor <diegovictorgonzaga@gmail.com>
2021-04-21 14:57:56 -0300

    Create message model

---
+++ src/entities/Message.ts
@@ -0,0 +1,40 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryColumn,
+} from 'typeorm';
+import { v4 as uuid } from 'uuid';
+import User from './User';
+
+@Entity('messages')
+class Message {
+  @PrimaryColumn()
+  id: string;
+
+  @Column()
+  user_id: string;
+
+  @ManyToOne(() => User)
+  @JoinColumn({ name: 'user_id' })
+  user: User;
+
+  @Column()
+  admin_id: string;
+
+  @Column()
+  text: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  constructor() {
+    if (!this.id) {
+      this.id = uuid();
+    }
+  }
+}
+
+export default Message;
