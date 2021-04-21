93eac547a39288415840e13cbe3a9681d62e4553
DiegoVictor <diegovictorgonzaga@gmail.com>
2021-04-21 13:26:36 -0300

    Configure users controller and route

--- src/routes.ts
+++ src/routes.ts
@@ -1,11 +1,14 @@
 import { Router } from 'express';
 
 import SettingsController from './controllers/SettingsController';
+import UsersController from './controllers/UsersController';
 
 const app = Router();
 
 const settingsController = new SettingsController();
+const usersController = new UsersController();
 
 app.post('/settings', settingsController.store);
+app.post('/users', usersController.store);
 
 export default app;
