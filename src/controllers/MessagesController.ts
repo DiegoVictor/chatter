e972dd7287f9746dbf9581a9bec62436f8a1f1d3
DiegoVictor <diegovictorgonzaga@gmail.com>
2021-04-21 15:07:40 -0300

    Create messages route and controller

---
+++ src/controllers/MessagesController.ts
@@ -0,0 +1,19 @@
+import { Request, Response } from 'express';
+import MessagesServices from '../services/MessagesServices';
+
+class MessagesController {
+  async store(request: Request, response: Response): Promise<Response> {
+    const { user_id, admin_id, text } = request.body;
+    const messagesServices = new MessagesServices();
+
+    const message = await messagesServices.store({
+      user_id,
+      admin_id,
+      text,
+    });
+
+    return response.json(message);
+  }
+}
+
+export default MessagesController;
--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import MessagesController from './controllers/MessagesController';
 
 import SettingsController from './controllers/SettingsController';
 import UsersController from './controllers/UsersController';
@@ -7,8 +8,10 @@ const app = Router();
 
 const settingsController = new SettingsController();
 const usersController = new UsersController();
+const messagesController = new MessagesController();
 
 app.post('/settings', settingsController.store);
 app.post('/users', usersController.store);
+app.post('/messages', messagesController.store);
 
 export default app;
