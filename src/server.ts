57c02f72cb422b0306b7c99b80b68effbfdaadbc
DiegoVictor <diegovictorgonzaga@gmail.com>
2021-04-23 21:06:00 -0300

    Import admin websocket

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import { http } from './app';
 import './websocket/client';
+import './websocket/admin';
 
 http.listen(process.env.PORT, () => {
   console.log(`Server running on port ${process.env.PORT}`);
