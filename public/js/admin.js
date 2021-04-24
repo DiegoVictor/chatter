7dc5c23a33b1f5aab157d24c763ab138fe8490fa
DiegoVictor <diegovictorgonzaga@gmail.com>
2021-04-24 03:40:22 -0300

    Create admin demo page

---
+++ public/js/admin.js
@@ -0,0 +1,103 @@
+const socket = io();
+let connectionsUsers = new Map();
+
+socket.on('admin_list_pending', (connections) => {
+  document.getElementById('list_users').innerHTML = '';
+
+  let template = document.getElementById('template').innerHTML;
+
+  connections.forEach((connection) => {
+    connectionsUsers.set(connection.socket_id, connection);
+    if (!connection.admin_id) {
+      const rendered = Mustache.render(template, {
+        email: connection.user.email,
+        id: connection.socket_id,
+      });
+      document.getElementById('list_users').innerHTML += rendered;
+    }
+  });
+});
+
+function call(id) {
+  const connection = connectionsUsers.get(id);
+  const template = document.getElementById('admin_template').innerHTML;
+
+  const rendered = Mustache.render(template, {
+    email: connection.user.email,
+    id: connection.user_id,
+  });
+
+  document.getElementById('supports').innerHTML += rendered;
+
+  const params = {
+    user_id: connection.user_id,
+  };
+
+  socket.emit('admin_user_in_support', params);
+
+  socket.emit('admin_list_messages_by_user', params, (messages) => {
+    const container = document.getElementById(
+      `allMessages${connection.user_id}`
+    );
+
+    messages.forEach((message) => {
+      const div = document.createElement('div');
+
+      div.className = 'admin_message_admin';
+      if (message.admin_id === null) {
+        div.className = 'admin_message_client';
+      }
+
+      div.innerHTML = `<span>${message.text}</span>`;
+
+      container.appendChild(div);
+    });
+  });
+}
+
+function sendMessage(id) {
+  const text = document.getElementById(`send_message_${id}`);
+
+  if (text.value.length > 0) {
+    const params = {
+      text: text.value,
+      user_id: id,
+    };
+
+    socket.emit('admin_send_message', params);
+
+    const container = document.getElementById(`allMessages${id}`);
+
+    const div = document.createElement('div');
+    div.className = 'admin_message_admin';
+    div.innerHTML = `<span>${params.text}</span>`;
+
+    container.appendChild(div);
+    text.value = '';
+
+    setTimeout(() => {
+      container.scrollTo(0, container.offsetHeight);
+    }, 50);
+  }
+}
+
+function closeChat(id) {
+  connectionsUsers.delete(id);
+  document.getElementById(`admin${id}`).remove();
+}
+
+socket.on('admin_receive_message', (data) => {
+  const connection = connectionsUsers.get(data.socket_id);
+  const container = document.getElementById(`allMessages${connection.user_id}`);
+
+  const div = document.createElement('div');
+
+  div.className = 'admin_message_client';
+  div.innerHTML += `<span>${data.message.text}</span>`;
+
+  container.appendChild(div);
+
+  setTimeout(() => {
+    container.scrollTo(0, container.offsetHeight);
+  }, 50);
+});
---
+++ src/websocket/admin.ts
@@ -0,0 +1,45 @@
+import { io } from '../app';
+import ConnectionsService from '../services/ConnectionsService';
+import MessagesServices from '../services/MessagesServices';
+
+io.on('connect', async (socket) => {
+  const connectionsService = new ConnectionsService();
+  const messagesServices = new MessagesServices();
+  const connectionsPending = await connectionsService.getPending();
+
+  io.emit('admin_list_pending', connectionsPending);
+
+  socket.on('admin_list_messages_by_user', async (params, callback) => {
+    const { user_id } = params;
+
+    const messages = await messagesServices.listByUserId(user_id);
+    callback(messages);
+  });
+
+  socket.on('admin_send_message', async (params) => {
+    const { user_id, text } = params;
+
+    const [{ socket_id }] = await Promise.all([
+      connectionsService.getByUserId(user_id),
+      messagesServices.store({
+        user_id,
+        text,
+        admin_id: socket.id,
+      }),
+    ]);
+
+    io.to(socket_id).emit('admin_sent_message', {
+      text,
+      socket_id: socket.id,
+    });
+  });
+
+  socket.on('admin_user_in_support', async (params) => {
+    const { user_id } = params;
+
+    await connectionsService.setAdminId(user_id, socket.id);
+    const connectionsPending = await connectionsService.getPending();
+
+    io.emit('admin_list_pending', connectionsPending);
+  });
+});
