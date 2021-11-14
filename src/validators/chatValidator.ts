b5255cb971e82a059ec8129bf1a15bb24823679f
DiegoVictor <diegovictorgonzaga@gmail.com>
2021-11-14 11:56:11 -0300

    Create chat validator

--- src/validators/chatValidator.ts
+++ src/validators/chatValidator.ts
@@ -1,9 +1,6 @@
 import { celebrate, Joi, Segments } from 'celebrate';
 
 export default celebrate({
-  [Segments.PARAMS]: Joi.object().keys({
-    username: Joi.string().required(),
-  }),
   [Segments.BODY]: Joi.object().keys({
     chat: Joi.boolean().required(),
   }),
